10b224bcb18907a3619475cd9abbc3011f1c3c99
Douglas <douglas.jesuscosta@outlook.com>
2019-04-27 12:53:38 -0300

    Adicionando modelagem de livro

--- models/Book.swift
+++ models/Book.swift
@@ -8,8 +8,44 @@
 
 import Foundation
 
-public class Book{
+class Book : NSObject, NSCoding {
     
+    let name:String
+    let author:String
+    let classification:Int
+    let bookDescription:String
+    let value:Float
     
+    init(name:String, author:String, classification:Int, bookDescription:String, value:Float) {
+        self.name = name
+        self.author = author
+        self.classification = classification
+        self.bookDescription = bookDescription
+        self.value = value
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.name = aDecoder.decodeObject(forKey : "name") as! String
+        self.author = aDecoder.decodeObject(forKey: "author") as! String
+        self.classification = aDecoder.decodeInteger(forKey: "classification")
+        self.bookDescription = aDecoder.decodeObject(forKey: "bookDescription") as! String
+        self.value = aDecoder.decodeObject(forKey: "value") as! Float
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(author, forKey : "author")
+        aCoder.encode(classification, forKey : "classification")
+        aCoder.encode(bookDescription, forKey: "bookDescription")
+        aCoder.encode(value, forKey: "value")
+    }
     
 }
+
+
+
+
+
+
+
+
